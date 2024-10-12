5162825e3d7b0c08f7858b1ff33dcaea0c9b7c2a
zuanet <168155305+zuanet@users.noreply.github.com>
2024-10-13 00:18:49 +0300

    Delete worker.js

--- worker.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-//Object.defineProperty(exports, "__esModule", { value: true });
-console.log("in worker");
-const {WorkerCore, workerLog} = require("./dist/lib/worker-core");
-let core = new WorkerCore();
-console.log("WorkerCore started", core);
-navigator.__core = core;
-navigator.__workerLog = workerLog;
-//workerLog.enable('all')
